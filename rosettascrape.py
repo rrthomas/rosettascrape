66507f020da1fe64c4207b4062c7ff6d8f908c01
Steve Birtles <sbirtles@farnborough.ac.uk>
2017-08-06 20:54:03 +0100

    Do 15 languages in parallel, save to folders.

--- rosettascrape.py
+++ rosettascrape.py
@@ -1,65 +1,124 @@
+from threading import Thread
 from lxml import html
 from lxml import etree
 import requests
+import os
+import os.path
 
-language = input ('Which language?')
-extension = input ('Which file extension?')
+languages = {'Kotlin': '.kt', 
+			 'C#': '.cs',
+			 'Ruby': '.rb', 
+			 'JavaScript': '.js', 
+			 'Lua': '.lua', 
+			 'Python': '.py', 
+			 'Java': '.java', 
+			 'C++': '.cpp', 			 
+			 'Go': '.go', 			 
+			 'Perl 6': '.p6',
+			 'PHP': '.php',
+			 'Groovy': '.groovy',
+			 'CoffeeScript': '.coffee',
+			 'Haskell', 'hs'}
 
-if (extension[0] != '.'):
-	extension = '.' + extension
+class RosettaScraper(Thread):
 
-page = requests.get('http://rosettacode.org/wiki/Category:{0}'.format(language.replace(' ', '_')))
-tree = html.fromstring(page.content)
+	def __init__(self, language, extension):
+		Thread.__init__(self)    
+		self.l = language
+		self.e = extension
 
-results = tree.xpath('//div[@id="mw-pages"]//a')
+	def run(self): 
 
-for it in results:	
-	filename = ''.join(c for c in it.text_content() if c.isalnum() or c in (' ','.','_')).rstrip() + extension
-	title = '{0} -> {1}'.format(it.text_content(), filename)
-	href = it.attrib['href']
-	print('\n{0}\n{1}\n{0}'.format('-' * len(title), title))
+		language = self.l
+		extension = self.e
 
-	subpage = requests.get('http://rosettacode.org' + href)
-	subtree = html.fromstring(subpage.content)
+		if not os.path.exists(language):
+			os.makedirs(language)
+	
+		page = requests.get('http://rosettacode.org/wiki/Category:{0}'.format(language.replace(' ', '_')))
+		tree = html.fromstring(page.content)
 
-	subresults = subtree.xpath('//*[@id="mw-content-text"]/*')
+		results = tree.xpath('//div[@id="mw-pages"]//a')
 
-	next_node = False
-	for node in subresults:
-		if next_node and node.tag == 'pre':
+		for it in results:	
+			description = it.text_content().replace('/', ' - ')
+			filename = language + '/' + ''.join(c for c in description if c.isalnum() or c in (' ','.','_')).rstrip() 
+
+			if os.path.isfile(filename + extension):
+				continue
+
+			href = it.attrib['href']
 			
-			code = etree.tostring(node, encoding='unicode', with_tail=False)			
+			subpage = requests.get('http://rosettacode.org' + href)
+			subtree = html.fromstring(subpage.content)
 
-			with open(filename, 'w') as f:
+			subresults = subtree.xpath('//*[@id="mw-content-text"]/*')
 
-				tag = ''
-				snippet = ''
-				in_tag = False
-				for char in code:
-					if char == '<':
-						in_tag = True
-						if tag.lower().startswith('br'):
-							print()
-							f.write('\n')
-						tag = ''
-					elif char == '>':
-						in_tag = False				
-						snippet = snippet.replace(chr(160), '')
-						snippet = snippet.replace('&gt;', '>')
-						snippet = snippet.replace('&lt;', '<')
-						snippet = snippet.replace('&amp;', '&')
-						print (snippet, end='')
-						f.write(snippet)					
-						snippet = ''
-					else:
-						if in_tag:
-							tag += char
-						else:
-							snippet += char		
+			additional = ''
+			process_node = False
+			for node in subresults:
 
-			break
+				if process_node:
+					
+					if node.tag == 'pre' or node.tag == 'code':
+					
+						code = etree.tostring(node, encoding='unicode', with_tail=False)			
 
-		if node.tag == 'h2' and node.text_content() == language:
-			next_node = True
+						title = '{0} : {1} -> {2}'.format(language, it.text_content(), filename + additional + extension)
+						print('\n{0}\n{1}\n{0}'.format('-' * len(title), title))
 
-	print ()	
+						with open(filename + additional + extension, 'w') as f:					
+
+							tag = ''
+							snippet = ''
+							in_tag = False
+							for char in code:
+								if char == '<':
+									in_tag = True
+									if tag.lower().startswith('br'):
+										print()
+										f.write('\n')
+									tag = ''
+								elif char == '>':
+									in_tag = False				
+									snippet = snippet.replace(chr(160), '')
+									snippet = snippet.replace('&gt;', '>')
+									snippet = snippet.replace('&lt;', '<')
+									snippet = snippet.replace('&amp;', '&')
+									print (snippet, end='')
+									f.write(snippet)					
+									snippet = ''
+								else:
+									if in_tag:
+										tag += char
+									else:
+										snippet += char		
+
+						if additional == '':
+							additional = ' i'
+						additional += 'i'
+
+					elif not node.tag == 'h2':
+
+						with open(filename + additional + '.txt', 'a') as f:
+
+							f.write(node.text_content())
+
+				if not process_node:
+					if node.tag == 'h2' and node.text_content() == language:
+						process_node = True
+				else:
+					if node.tag == 'h2' and not node.text_content() == language:
+						break
+
+			print ()	
+
+
+scanners = []
+
+for language, extension in languages.items():
+	scanner = RosettaScraper(language, extension)
+	scanner.start()
+
+for scanner in scanners:
+	scanner.join()
